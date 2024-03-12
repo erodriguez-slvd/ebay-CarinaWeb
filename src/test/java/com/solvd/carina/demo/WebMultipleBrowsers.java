7fbd5711f148400cf01641109eee4ce3630b0439
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-03-12 15:43:51 -0300

    Add selenium grid implementation. Add Firefox and Chrome browsers

--- src/test/java/com/solvd/carina/demo/WebMultipleBrowsers.java
+++ src/test/java/com/solvd/carina/demo/WebMultipleBrowsers.java
@@ -2,7 +2,11 @@ package com.solvd.carina.demo;
 
 import com.solvd.carina.demo.pages.HomePage;
 import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.webdriver.core.capability.impl.desktop.FirefoxCapabilities;
 import com.zebrunner.carina.webdriver.core.capability.impl.desktop.SafariCapabilities;
+import io.appium.java_client.safari.SafariDriver;
+import io.appium.java_client.safari.options.SafariOptions;
+import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -11,13 +15,15 @@ import org.testng.annotations.Parameters;
 import java.lang.invoke.MethodHandles;
 
 public class WebMultipleBrowsers implements IAbstractTest {
+    WebDriver driver;
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     @Parameters("browser")
     @BeforeTest
     public void setUp(String browser){
         if (browser.equalsIgnoreCase("firefox")) {
             LOGGER.info("Executing on Firefox");
-            HomePage firefoxHomePage = new HomePage(getDriver(browser));
+            getDriver(browser, new FirefoxCapabilities().getCapability("Firefox Browser"));
+            HomePage firefoxHomePage = new HomePage(getDriver(browser)); // return named "firefox" driver from the pool to init HomePage.
             firefoxHomePage.open();
             Assert.assertTrue(firefoxHomePage.isPageOpened(), "Firefox home page is not opened!");
         } else if (browser.equalsIgnoreCase("chrome")) {
@@ -25,12 +31,7 @@ public class WebMultipleBrowsers implements IAbstractTest {
             HomePage chromeHomePage = new HomePage(getDriver(browser));
             chromeHomePage.open();
             Assert.assertTrue(chromeHomePage.isPageOpened(), "Chrome home page is not opened!");
-        } else if (browser.equalsIgnoreCase("safari")) {
-            LOGGER.info("Executing on Safari");
-            HomePage safariHomePage = new HomePage(getDriver(browser, new SafariCapabilities().getCapability("Safari Browser")));
-            safariHomePage.open();
-            Assert.assertTrue(safariHomePage.isPageOpened(), "Safari home page is not opened!");
-        }else {
+        } else {
             throw new IllegalArgumentException("The Browser Type is Undefined");
         }
     }
